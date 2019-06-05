1f81c1d31f74488b1919102c3a336d470d028b52
Magdiel <mgdlgj@gmail.com>
2019-06-05 18:41:06 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
 #wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
 #wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
