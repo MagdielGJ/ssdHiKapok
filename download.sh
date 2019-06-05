11b898bdfeacd8492a9d354ddc9183ac43a52f21
Magdiel <mgdlgj@gmail.com>
2019-06-05 18:38:17 -0500

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,15 @@
+#wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+#wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+#wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+
+wget -c http://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget -c http://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget -c http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
+
+mkdir -p VOCROOT
+
+(cd VOCROOT && tar xf ../VOCtrainval_06-Nov-2007.tar)
+(cd VOCROOT && tar xf ../VOCtrainval_11-May-2012.tar)
+(cd VOCROOT && tar xf ../VOCtest_06-Nov-2007.tar)
+
+
